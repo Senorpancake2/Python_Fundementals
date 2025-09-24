0dee11da3fa39cf0ce7f5f4eb2cad68735a065ad
Senorpancake2 <Jadenhasenfus557@gmail.com>
2025-09-24 16:18:07 +0000

    Lesson 1 Datatypes

---
+++ lesson01_datatypes.py
@@ -0,0 +1,25 @@
+# 1. STRINGS:
+
+print("once upon a time\nthere was a time")
+print("\n")
+print("once upon a time\tthere was a time")
+# 2. Integers
+print(41)
+print(-23)
+print(62+11-6)
+
+# 3. FLOAT
+print(4.0)
+print(2.0+4.5)
+print(2.0+4)
+
+#BOOLEAN = True/False
+print(True) #1
+print(False) #0
+print(1==2)
+print(1!=2)
+#TYPE COMMAND
+print(type("string"))
+print(type("1"))
+print(type(1))
+print(type(1.0))
