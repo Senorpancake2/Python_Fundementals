06084abf243d035e1f78467e2fb26f613b10ef49
Senorpancake2 <Jadenhasenfus557@gmail.com>
2025-09-26 18:00:29 +0000

    Math Operators lesson

---
+++ lesson02_variables.py
@@ -0,0 +1,49 @@
+#CONCEPTS VARIABLES
+
+taco = "Hello world"
+
+Password = "P@ssw0rd"
+
+print("Password: ",Password)
+
+
+tempature = 72.5
+print (type(tempature))
+
+
+# for math problems
+
+x= 3.14
+y= 7
+
+print(x+y)
+
+# variables are flexible, you create or update another variable like so:
+count = 10
+print(count)
+count_down = count - 1
+print(count_down)
+count = count_down
+print(count)
+count_down = count - 1
+print(count_down)
+
+
+Name = "Radia Perlam"
+Age = 34
+Occupation = "Networking Engineer"
+
+Count = 10
+add = 5
+print(Count+add)
+
+x = 4
+y = "hello"
+z = x
+x = y
+y = z
+print (y)
+
+is_raining = False
+is_raining = True
+print ("Is it raining?:", is_raining)
---
+++ lesson03_math.py
@@ -0,0 +1,31 @@
+#KEY CONCEPTS: math operators: +,-,x,/,//,%,
+
+add = 7 + 2
+print ("Sum:", add)
+
+subtract = 7 - 2
+print ("Difference:", subtract)
+
+multiply = 7 * 2
+print ("Product:", multiply)
+
+float_divide = 7 / 2
+print ("Quotient:", float_divide)
+
+integer_divide = 7//2
+print ("Integer division:", integer_divide)
+
+modulus = 7 % 2 #modulus gives the remainder
+print ("Modulus:", modulus)
+
+exponent = 7**2
+print("Exponent:", exponent)
+
+print(8*4+5==4**3-25-2)
+
+result = 10-2+3
+print("Result:", result)
+
+result2 = 5+2**3*(4-1)
+print("Result2:", result2)
+
