e651fb05a977977891fe45f3572d5003e0b5b917
Senorpancake2 <Jadenhasenfus557@gmail.com>
2025-09-30 16:28:17 +0000

    Lesson 4

--- lesson04_libraries.py
+++ lesson04_libraries.py
@@ -7,3 +7,20 @@ print ("Round up:", math.ceil(4.2))
 print("Round down:", math.floor(4.2))
 print("Exponent",math.pow(4,3))
 print("Pi",math.pi)
+
+#------------------
+#  RANDOM LIBRARY
+#------------------
+
+seed = 67902343
+print("Step #1:",math.sqrt(seed))
+step_1 = 8240.287798372094
+step_2 = step_1/47
+print("Step #2:",step_2 )
+result2 = 175.32527230578924
+step_3 = result2**4
+print("Step #3:", step_3)
+step_4 = 944883115.2875878
+print("Step #4:",math.floor(step_4))
+step_5 = 944883115
+result = step_5
