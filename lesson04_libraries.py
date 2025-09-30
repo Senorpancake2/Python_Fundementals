8d29d1c804c36589784e4d76b0985f675aca4af9
Senorpancake2 <Jadenhasenfus557@gmail.com>
2025-09-30 15:54:04 +0000

    Lesson 4: Math Library

---
+++ lesson04_libraries.py
@@ -0,0 +1,9 @@
+#Python libraries are repositories of code that you can use in your own
+
+import math
+print("\n----MATH LIBRARY----\n")
+print("Square root:",math.sqrt(25))
+print ("Round up:", math.ceil(4.2))
+print("Round down:", math.floor(4.2))
+print("Exponent",math.pow(4,3))
+print("Pi",math.pi)
