b6365daaa41729ab2ba36fa3362ee0cc3ca2937f
Senorpancake2 <Jadenhasenfus557@gmail.com>
2025-10-08 16:52:56 +0000

    Lesson06 part #2

--- lesson06_strings.py
+++ lesson06_strings.py
@@ -4,13 +4,14 @@ name = "Ada"
 print(greeting, name)
 
 
-
+#CONCATENATION
 message = greeting + " " + name + "!"
 print("Concatenated message", message)
 
 second_letter = greeting[1]
 print(second_letter)
 
+#SPLICING
 word = "Supercalifragilisticexpialidocious"
 print("First letter:", word[0])
 print("Last letter:", word[-1])
@@ -33,6 +34,24 @@ word_length = len(word)
 print(word_length)
 
 phrase = "spongEBOB"
-print("\nUppercase", phrase.upper())
-print("Lowercase",phrase.lower())
-print("Capitalize",phrase.capitalize())
+print("\nUppercase:", phrase.upper())
+print("Lowercase:",phrase.lower())
+print("Capitalize:",phrase.capitalize())
+print("Title Format:", phrase.title())#Makes every word capitalized
+
+print("\n---The Replace Function---\n")
+sentence = "I'm a goofy goober"
+new_sentence = sentence.replace("I'm","You're")
+print(sentence)
+print(new_sentence)
+
+print("\n----Formatted Strings----\n")
+
+name = "Ada"
+age = 28
+city = "London"
+print(f"Hello, my name is {name}. I am {age} years old. and live in {city}")
+
+# f-strings can do math and function calls inside {}
+
+print(f"Next year, I'll be {age + 1}. My name in uppercase is {name.upper()}.")
