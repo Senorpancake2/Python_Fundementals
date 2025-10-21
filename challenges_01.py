b2a94ac94758136469c5922f971adb843fad0c83
Senorpancake2 <Jadenhasenfus557@gmail.com>
2025-10-21 17:10:36 +0000

    challenges fixes

--- challenges_01.py
+++ challenges_01.py
@@ -1,24 +1,24 @@
-# #CHALLENGE_1: PALIDROME
-# word = input("Enter a word: ")
-# if word == word[::-1]: print("Your word is a palidrome")
-# else: print("Your word is not a palidrome")
+#CHALLENGE_1: PALIDROME
+word = input("Enter a word: ")
+if word == word[::-1]: print("Your word is a palidrome")
+else: print("Your word is not a palidrome")
 
 
-# #Challenge_2: Email
-# email = input("Enter your email: ")
-# index = email.index("@")
-# print(index)
-# print(email[index+1:])
+#Challenge_2: Email
+email = input("Enter your email: ")
+index = email.index("@")
+print(index)
+print(email[index+1:])
 
 
 
-# #Challenge_3: First and last match
+#Challenge_3: First and last match
 
-# list = ["Apple","Bannana","Orange"]
-# print(list)
-# response= input("Enter the last element in the list: ")
-# if response == list[2]:print(True)
-# else: print(False)
+list = ["Apple","Bannana","Orange"]
+print(list)
+response= input("Enter the last element in the list: ")
+if response == list[2]:print(True)
+else: print(False)
 
 
 #Challenge_4: Suffix adder
